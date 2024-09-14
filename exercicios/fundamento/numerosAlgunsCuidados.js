049e5f7b94b7752fb4818d4fe497f386201720ef
Gustavo Falcão <gustavo.ofalcao6@gmail.com>
2024-09-14 00:09:11 -0300

    Commit fundamentos

---
+++ exercicios/fundamento/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7/0)
+console.log('3' + 2) // String ganha, realiza a concatenação
+console.log(3 - 2)
+console.log("10"/2) //Se ele reconhecer a String, ele transforma em númerico
+console.log("Show!" * 2) // Algumas lingugagens multiplica a String, aparecendo ela multiplas vezes
+console.log(0.7 + 0.1)
+console.log((10.345).toFixed(2))
